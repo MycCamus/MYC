725d08456661cceaadb9f5349f8c7df526b1f6db
MycCamus <2675256284@qq.com>
2025-02-25 20:25:18 +0800

    first commit

--- train.py
+++ train.py
@@ -2,14 +2,14 @@ import warnings
 warnings.filterwarnings("ignore")
 from ultralytics import YOLO
 
-model_yaml_path = 'ultralytics/cfg/models/v8/yolov8n-CBAM.yaml'
+model_yaml_path = 'ultralytics/cfg/models/v8/yolov8.yaml'
 
 data_yaml_path = 'ultralytics/cfg/datasets/safety_helmet.yaml'
 
 pre_model_name = 'yolov8s.pt'
 
 if __name__ == '__main__':
-    model = YOLO(model='ultralytics/cfg/models/v8/yolov8n-CBAM.yaml').load(pre_model_name)
+    model = YOLO(model='ultralytics/cfg/models/v8/yolov8.yaml').load(pre_model_name)
     results = model.train(data = data_yaml_path,
                           epochs = 50,
                           batch = 16,
@@ -17,5 +17,5 @@ if __name__ == '__main__':
                           device = 0,
                           close_mosaic = 10,
                           project = 'runs/train',
-                          name='train_v8OCBAMTEST',
+                          name='train_v8',
                           )
